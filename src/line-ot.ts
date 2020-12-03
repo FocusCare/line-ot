e6dc7f45e55eac4c3d93cba717a4f3f0360c66d8
冯超 <fengchao@youzan.com>
2020-12-03 17:05:48 +0800

    feat: 完成retain开发和单测

--- src/line-ot.ts
+++ src/line-ot.ts
@@ -1,6 +1,163 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+type RetainOperation = number;
+type InsertOperation = string;
+type DeleteOperation = number;
 
+type Operation = RetainOperation | InsertOperation | DeleteOperation;
+
+const CHANGE_LINE_CHAR = '\n';
+
+class LineOT {
+  ops: Operation[] = [];
+
+  baseLineCounts: number = 0;
+
+  targetLineCounts: number = 0;
+
+  constructor(ops?: Operation[]) {
+    if (ops && ops.length > 0) {
+      this.fromJSON(ops);
+    }
+  }
+
+  isRetain = (op: Operation) => {
+    return typeof op === 'number' && op > 0;
+  };
+
+  isInsert = (op: Operation) => {
+    return typeof op === 'string';
+  };
+
+  isDelete = (op: Operation) => {
+    return typeof op === 'number' && op < 0;
+  };
+
+  retain = (n: RetainOperation) => {
+    // tslint:disable-next-line: strict-type-predicates
+    if (typeof n !== 'number') {
+      throw new Error('retain expects an integer');
+    }
+    if (n === 0) {
+      return this;
+    }
+    this.baseLineCounts += n;
+    this.targetLineCounts += n;
+    const beforeOp = this.ops[this.ops.length - 1];
+
+    if (this.isRetain(beforeOp)) {
+      // 操作符合并逻辑
+      // retain(1),retain(1) => retain(2)
+      this.ops[this.ops.length - 1] = (beforeOp as number) + n;
+    } else {
+      this.ops.push(n);
+    }
+    return this;
+  };
+
+  insert = (str: InsertOperation) => {
+    // tslint:disable-next-line: strict-type-predicates
+    if (typeof str !== 'string') {
+      throw new Error('insert expects a string');
+    }
+    if (str === '') {
+      return this;
+    }
+    this.targetLineCounts++;
+    const ops = this.ops;
+    const beforeOp = ops[ops.length - 1];
+    if (this.isDelete(beforeOp)) {
+      // 操作符合并逻辑
+      // 如果前一个是DeleteOperation
+      // delete(2),insert('a') => delete(1)
+      // delete(1),insert('a') => noop
+      const deleteLineCounts = (beforeOp as number) - 1;
+      if (deleteLineCounts === 0) {
+        ops.pop();
+      } else {
+        ops[ops.length - 1] = deleteLineCounts;
+      }
+    } else {
+      ops.push(str);
+    }
+    return this;
+  };
+
+  delete = (n: DeleteOperation) => {
+    // tslint:disable-next-line: strict-type-predicates
+    if (typeof n !== 'number') {
+      throw new Error('delete expects an integer or a string');
+    }
+    if (n === 0) {
+      return this;
+    }
+    if (n > 0) {
+      n = -n;
+    }
+    this.baseLineCounts -= n;
+    const ops = this.ops;
+
+    const beforeOp = ops[ops.length - 1];
+
+    if (this.isDelete(beforeOp)) {
+      this.ops[this.ops.length - 1] = (beforeOp as number) + n;
+    } else {
+      this.ops.push(n);
+    }
+    return this;
+  };
+
+  isNoop = (): boolean => {
+    return this.ops.length === 0 || (this.ops.length === 1 && this.isRetain(this.ops[0]));
+  };
+
+  toJSON = () => {
+    return this.ops;
+  };
+
+  toString = () => {
+    return this.toJSON().toString();
+  };
+
+  fromJSON = (ops: Operation[]) => {
+    return ops.map(op => {
+      if (this.isRetain(op)) {
+        this.retain(op as RetainOperation);
+      } else if (this.isInsert(op)) {
+        this.insert(op as InsertOperation);
+      } else if (this.isDelete(op)) {
+        this['delete'](op as DeleteOperation);
+      } else {
+        throw new Error('unknown operation: ' + JSON.stringify(op));
+      }
+    });
+  };
+
+  apply = (file: string) => {
+    const lines = file.split(CHANGE_LINE_CHAR);
+
+    if (lines.length !== this.baseLineCounts) {
+      throw new Error("The operation's base line counts must be equal to the file's line counts.");
+    }
+
+    let resultFile: string[] = [];
+    let indexOfLines = 0;
+    this.ops.forEach(op => {
+      if (this.isRetain(op)) {
+        if (indexOfLines + (op as number) > lines.length) {
+          throw new Error("Operation can't retain more line counts than are left in the file.");
+        }
+        const endIndex = indexOfLines + (op as number);
+        resultFile = resultFile.concat(lines.slice(indexOfLines, endIndex));
+        indexOfLines = endIndex;
+      } else if (this.isInsert(op)) {
+        resultFile.push(op as string);
+      } else {
+        // delete op
+        indexOfLines -= op as number;
+      }
+    });
+
+    return resultFile.join(CHANGE_LINE_CHAR);
+  };
 }
+
+export default LineOT;
--- test/line-ot.test.ts
+++ test/line-ot.test.ts
@@ -1,14 +1,127 @@
-import DummyClass from "../src/line-ot"
+import LineOT from '../src/line-ot';
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+const file = `a
+b
+c`;
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+const fileLineCounts = file.split('\n').length;
+
+describe('constructor', () => {
+  it('success', () => {
+    const ot = new LineOT([fileLineCounts]);
+    ot.apply(file);
+    expect(ot.toJSON()[0] === fileLineCounts).toBeTruthy();
+  });
+
+  it('error', () => {
+    const ot = new LineOT([fileLineCounts + 1]);
+    let result;
+    let error = '';
+    try {
+      result = ot.apply(file);
+    } catch (e) {
+      error = e;
+      result = '';
+    }
+    expect(result === '').toBeTruthy();
+    expect(error !== '').toBeTruthy();
+  });
+});
+
+describe('retain', () => {
+  it('params 0', () => {
+    const ot = new LineOT();
+    let error = '';
+    try {
+      ot.retain(0);
+    } catch (e) {
+      error = e;
+    }
+    expect(error === '').toBeTruthy();
+    expect(ot.toJSON().length === 0).toBeTruthy();
+  });
+
+  it('whole counts', () => {
+    const ot = new LineOT();
+    let result;
+    try {
+      result = ot.retain(fileLineCounts).apply(file);
+    } catch (e) {
+      result = '';
+    }
+    expect(result === file).toBeTruthy();
+  });
+
+  it('merge retain', () => {
+    const ot = new LineOT();
+    let result;
+    try {
+      result = ot
+        .retain(1)
+        .retain(2)
+        .apply(file);
+    } catch (e) {
+      result = '';
+    }
+    expect(result === file).toBeTruthy();
+    expect(ot.toString() === fileLineCounts.toString()).toBeTruthy();
+  });
+
+  it('out of line counts', () => {
+    const ot = new LineOT();
+    let result;
+    let error = '';
+    try {
+      result = ot.retain(4).apply(file);
+    } catch (e) {
+      error = e;
+      result = '';
+    }
+    expect(result === '').toBeTruthy();
+    expect(error !== '').toBeTruthy();
+  });
+
+  it('out of line counts by merge', () => {
+    const ot = new LineOT();
+    let result;
+    let error = '';
+    try {
+      result = ot
+        .retain(2)
+        .retain(2)
+        .apply(file);
+    } catch (e) {
+      error = e;
+      result = '';
+    }
+    expect(result === '').toBeTruthy();
+    expect(error !== '').toBeTruthy();
+    expect(ot.toString() === '4').toBeTruthy();
+  });
+
+  it('less then line counts', () => {
+    const ot = new LineOT();
+    let result;
+    let error = '';
+    try {
+      result = ot.retain(2).apply(file);
+    } catch (e) {
+      error = e;
+      result = '';
+    }
+    expect(result === '').toBeTruthy();
+    expect(error !== '').toBeTruthy();
+  });
+
+  it('params error', () => {
+    const ot = new LineOT();
+    let error = '';
+    try {
+      // @ts-ignore
+      ot.retain('a');
+    } catch (e) {
+      error = e;
+    }
+    expect(error !== '').toBeTruthy();
+  });
+});
